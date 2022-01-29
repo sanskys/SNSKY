f7e2d64381ad5bbab9658bd23fed9b7aee3a99b4
sandy4de <80989645+sandy4de@users.noreply.github.com>
2022-01-29 20:57:46 +0100

    Update script.sh

--- SlotLeader/script.sh
+++ SlotLeader/script.sh
@@ -25,13 +25,10 @@ echo "Epoch: $EPOCH, SLOTS: $SLOTS"
 
 ######################################################################################
 AT=$(jq '.[].at' <<< $ASSIGNED | sed  -e 's/.......$//' -e 's/T/ /g' -e 's/"/''/g')
-SLOT=$(jq '.[].slotInEpoch' <<< $ASSIGNED)
+SLOT=$(jq '.[].slot' <<< $ASSIGNED)
 NO=$(jq '.[].no' <<< $ASSIGNED)
 
-paste <(echo "$AT") <(echo "$SLOT") <(echo "") <(echo "$NO") --delimiters , > temp.csv
-awk -F ',' -v OFS=',' '$2 < 50000 { $3 = 50000 }1' temp.csv > slot.csv
-rm temp.csv
-
-sed -i '1 i\Time,Slot,Height,No' slot.csv
+paste <(echo "$AT") <(echo "$SLOT") <(echo "$NO") --delimiters , > slot.csv
+sed -i '1 i\Time,Slot,No' slot.csv
 cat slot.csv
 #######################################################################################
